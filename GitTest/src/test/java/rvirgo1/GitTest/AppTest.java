3f87166ccc5b5987793d289b6b48ae642109df55
rvirgo1 <35464981+rvirgo1@users.noreply.github.com>
2018-01-15 15:35:26 -0700

    change the url so the test would pass

--- GitTest/src/test/java/rvirgo1/GitTest/AppTest.java
+++ GitTest/src/test/java/rvirgo1/GitTest/AppTest.java
@@ -12,17 +12,19 @@ protected WebDriver driver;
 	public void guru99tutorial() throws InterruptedException {
 		  System.setProperty("webdriver.chrome.driver", "d:/Program Files/Drivers/chromedriver.exe");
 		  driver = new ChromeDriver();
-		  String eTitle = "Meet Guru99";
+		  String eTitle = "Guru - Hire Quality Freelancers And Find Freelance Jobs";
 		  String aTitle = "";
 		  //launch chrome and redirect to base url
 		  driver.get("http://www.guru.com");
 		  driver.manage().window().maximize();
 		  aTitle = driver.getTitle();
+		  System.out.println("Page Title: "+aTitle);
 		  if (aTitle.equals(eTitle)) {
 			  System.out.println("Test Passed");
 		  } else {
 			  System.out.println("Test Failed");
 		  }
+		  System.out.println("at last the test has ended");
 		  Thread.sleep(3000);
 		  driver.close();
 	}
